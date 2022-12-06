9aae5946c446ebdaa1f6ac6c46693d08986f344a
Carol Cheung <carol.cheung@telus.com>
2022-12-05 20:19:29 -0800

    added character counter change to red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
   $('#tweet-text').keydown(function () {  //this is refering to tweet-text use jquery instead of counter
-    const currentLength = $(this).val().length
-    const characterCounter = $(this).siblings().children()[1]
+    const currentLength = $(this).val().length;
+    const characterCounter = $(this).siblings().children()[1];
 
-    $(characterCounter).text(140 - currentLength)
+    $(characterCounter).text(140 - currentLength);
+
+    if (currentLength > 140) {
+      $(characterCounter).css('color', '#FF0000');
+    } else {
+      $(characterCounter).css('color', '#545149');
+    }
   });
 });
 
